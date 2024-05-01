cca7dd68d4f5d89088101b800dc2e5078d1a02f8
WymanWMTan <WymanTan@qq.com>
2024-05-01 16:14:19 +0800

    generalize py file importing

--- Main.py
+++ Main.py
@@ -1,16 +1,13 @@
-import sys
-sys.path.append(r"Z:\\My CodingWorks\\Workspace")
-
-from JellyfinSortTitleAutoFilling.ListNFOFile import find_files_with_suffix
-from JellyfinSortTitleAutoFilling.ReadNFOFile import Read_NFO_tag_value
-from JellyfinSortTitleAutoFilling.TranslateTitleToPinyin import to_pinyin
-from JellyfinSortTitleAutoFilling.UpdateNFOFile import write_NFO_tag_value
+from ListNFOFile import find_files_with_suffix
+from ReadNFOFile import Read_NFO_tag_value
+from TranslateTitleToPinyin import to_pinyin
+from UpdateNFOFile import write_NFO_tag_value
 
 # 指定的电影或者电视文件夹，目标找到里面包含的所有.nfo文件
-directory_path = "G:\\My Movies"
+directory_path = "G:\\TEMP"
 suffix = '.nfo'
-files = find_files_with_suffix(directory_path, suffix)
 
+files = find_files_with_suffix(directory_path, suffix)
 
 # 指定读取标签名称
 readTagName = 'title'
@@ -27,6 +24,7 @@ writeTagValue = ''
 中文翻译成拼音字符串
 把译文写入sorttitle标签, 插入或者覆盖
 """
+
 for file in files:
     print("###")
     print("---Start to update .nfo file '"+file+"'")
